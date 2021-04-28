8af4d8f54c0925041f8f7c45c2a57cae85f5621d
Edith Lopez <edith.lopez@minu.mx>
2021-04-27 21:58:16 -0500

    fix: modify api and add credentials

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'sey81ng0hl'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-edith-le.us.auth0.com',            // Auth0 domain
+  clientId: 'SAvKDZCRChC2k8jS2ZaVHVhSSiUOase7',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
